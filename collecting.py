595c8ff207b8e749f23f9c31ca98809a66b16342
azn-abel <66392457+azn-abel@users.noreply.github.com>
2021-04-10 11:42:35 -0400

    added status

--- collecting.py
+++ collecting.py
@@ -1,5 +1,9 @@
 from bot import *
 
+import pickle
+from database_management import *
+import pokemon
+
 
 class Collector:
     instances = []
@@ -29,3 +33,62 @@ class Channel:
         self.drop_active = False
         # print(self.__class__.instances)
         # print(self.__class__.instance_dict)
+
+
+@client.command()  # FIXED FOR SQL
+async def register(ctx):
+    author_id = ctx.message.author.id
+    for x in Collector.instances:
+        if author_id == x.id:
+            await ctx.reply("You are already registered to collect!")
+            return
+    x = Collector(author_id)
+    # fileObject = open(f'Collector Data/{x.id}.pickle', 'wb')
+    # pickle.dump(x, fileObject)
+    # fileObject.close()
+    pickle_string = pickle.dumps(x)
+    cur.execute("INSERT INTO Collectors (id, instance) VALUES(%s, %s)", (str(author_id), pickle_string,))
+    conn.commit()
+
+    await ctx.reply("You are now registered to collect!")
+    '''
+    y = pickle.load(open(f'Collector Data/{x.id}.pickle', 'rb'))
+    print(y.id)
+    print(y)
+    '''
+
+@client.command()  # FIXED FOR SQL, NEEDS TESTING
+async def get(ctx, arg):
+    if ctx.message.author.id not in special_privileges:
+        await ctx.reply("You dumbass.")
+        return
+
+    try:
+        # collector = Collector.instances_dict[ctx.message.author.id]
+        cur.execute("SELECT * FROM collectors WHERE id = %s;", (str(ctx.message.author.id),))
+        retrieved_pickle = cur.fetchone()[1]
+        collector = pickle.loads(retrieved_pickle)
+    except:
+        await ctx.reply("You are not a registered collector!")
+        return
+
+    poke = arg.lower()
+    if poke in pokemon.all_pokemon:
+        collector.pokemon_list.append(poke)
+        collector.unique_list.append(poke) if str(
+            poke) not in collector.unique_list else collector.dupe_list.append(poke)
+
+        # fileObject = open(f'Collector Data/{instance.id}.pickle', 'wb')
+        # pickle.dump(instance, fileObject)
+        # fileObject.close()
+
+        print(len(collector.unique_list))
+        pickle_string = pickle.dumps(collector)
+
+        cur.execute("UPDATE collectors SET instance = %s WHERE id = %s", (pickle_string, str(collector.id)))
+        conn.commit()
+        await ctx.reply(f"You have acquired {poke.capitalize()}!")
+        return
+    else:
+        await ctx.reply("That is not a valid Pokemon!")
+        return
